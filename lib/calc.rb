243b8c7d602aa992d2f3fae4a6c489722e6ffaa8
lbreguet <louis-w@live.com>
2017-02-10 16:54:09 -0500

    Challenge in progress
    -finish soon

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,37 @@
 # frozen_string_literal: true
 
 # Calculator Class here
+class Calculator
+  def initialize(start_num)
+    @num = start_num.to_f
+    self
+  end
+
+  def add(num)
+    @num += num
+    self
+  end
+
+  def subtract(num)
+    @num -= num
+    self
+  end
+
+  def multiply(num)
+    @num *= num
+    self
+  end
+
+  def divide(num)
+    @num /= num
+    self
+  end
+
+  def equals
+    @num
+  end
+
+  def clear
+    @num = 0
+  end
+end
